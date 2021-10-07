dc9f00d25964a39fec40f9f3b00dea204e3c8df7
Vlad Hapon <vladhpn@gmail.com>
2021-10-07 17:47:17 +0300

    update

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 const slides = document.querySelectorAll('.slide');
 
+window.addEventListener('keydown', (event) => {
+  if (event.code === 'Escape') {
+    clearActiveClasses();
+  }
+});
+
 for (const slide of slides) {
   slide.addEventListener('click', () => {
     clearActiveClasses();
-    // hundleClick();
     slide.classList.add('active');
-
-    console.log('click');
   });
 }
 
 function clearActiveClasses() {
   slides.forEach((slide) => {
     slide.classList.remove('active');
-  });
-}
-
-function hundleClick() {
-  slides.forEach((slide) => {
-    slide.addEventListener('click', () => {
-      slide.classList.toggle('active');
-    });
   });
 }
